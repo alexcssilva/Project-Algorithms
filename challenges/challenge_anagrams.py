1101a4f65f322d1a4e6072dbb91d53b4098a3a0a
Alex Silva <alexcssilva@gmail.com>
2022-10-10 22:17:38 -0300

    fix: linter

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,7 +1,2 @@
 def is_anagram(first_string, second_string):
-    try:
-        if low_index == high_index or low_index > high_index:
-            return True
-    except TypeError:
-        return None
-    return is_anagram(first_string, second_string)
+    """Faça o código aqui."""
