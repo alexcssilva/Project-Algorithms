e73da8ba17ad8dfe086338bdc07d7d526c592376
Alex Silva <alexcssilva@gmail.com>
2022-10-10 22:15:22 -0300

    fix: added challenge_palindromes_recursive.py - Desafio 02 concluido

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,7 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
+    try:
+        if low_index == high_index or low_index > high_index:
+            return True
+    except TypeError:
+        return None
+    return is_anagram(first_string, second_string)
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,7 +1,8 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    try:
-        if low_index == high_index or low_index > high_index:
-            return True
-    except TypeError:
-        return None
+    if word == '':
+        return False
+    if word[low_index] not in word[high_index]:
+        return False
+    if low_index == high_index or low_index > high_index:
+        return True
     return is_palindrome_recursive(word, low_index + 1, high_index - 1)
