85163f26ebf91be8f4b9d07fe85c83271828419b
Alex Silva <alexcssilva@gmail.com>
2022-10-10 21:38:18 -0300

    feat: added challenge_palindromes_recursive.py - Desafio 02 concluido

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,7 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
+    try:
+        if low_index == high_index or low_index > high_index:
+            return True
+    except TypeError:
+        return None
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
