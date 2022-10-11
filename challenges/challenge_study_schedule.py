2eaaf39652fffdca94dcc22cee6d02e3c35fff5f
Alex Silva <alexcssilva@gmail.com>
2022-10-10 21:19:22 -0300

    feat: added challange_study_schedule.py - Desafio 01 concluido

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    students = 0
+    try:
+        for hour in permanence_period:
+            if hour[0] <= target_time and hour[1] >= target_time:
+                students += 1
+    except TypeError:
+        return None
+
+    return students
