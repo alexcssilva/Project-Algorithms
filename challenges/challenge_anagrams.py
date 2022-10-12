2b2571f3cb45187ccc60273b1c2f20e19754c6cd
Alex Silva <alexcssilva@gmail.com>
2022-10-11 22:41:58 -0300

    feat: added challenge_anagrams.py - Desafio 03 concluido

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,9 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
+    first_word = first_string.lower()
+    second_word = second_string.lower()
+    for row in first_word:
+        if row in second_word:
+            second_word = second_word.replace(row, "", 1)
+        else:
+            return False
+    return len(second_word) == 0
